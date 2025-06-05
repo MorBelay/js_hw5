46d1a87b3d7fe7d98e7de9dc8556dc5fa5e021ec
MorBelay <morbelayev@gmail.com>
2025-06-05 20:13:23 +0300

    Add HTML.txt + js DOM functions (HOMEWORK)

---
+++ jshw5dom.js
@@ -0,0 +1,24 @@
+// //Task 1
+
+// document.getElementById("hoverBox").addEventListener("mouseover", function () {
+//   this.style.backgroundColor = "lightgreen";
+// });
+
+
+
+// //Task 2
+
+// document.getElementById("hoverBox2").addEventListener("mouseout", function () {
+//   this.style.backgroundColor = "gray";
+// });
+
+
+
+// //Task 3
+
+// let count = 0;
+
+// document.getElementById("clickBtn").addEventListener("click", function () {
+//   count++;
+//   document.getElementById("clickCount").textContent = count;
+// });
